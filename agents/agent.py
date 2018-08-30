b1c15f1f45656cb19d9f27d64ec632ac45f13016
Mike Roncone <mjroncone@gmail.com>
2018-08-30 14:08:40 -0500

    Get personal agent workin in notebook as copy of example agent

---
+++ agents/agent.py
@@ -0,0 +1,7 @@
+import numpy as np
+from task import Task
+from agents.policy_search import PolicySearch_Agent
+
+# Implementation of the DDPG algorithm described here: https://arxiv.org/pdf/1509.02971.pdf
+class DDPG_Agent(PolicySearch_Agent):
+    pass
